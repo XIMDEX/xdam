62a5b3c2c546adff709d6367cf76390e92206409
Alvaro Rosado <alvaro.rosado93@gmail.com>
2021-01-22 19:24:32 +0100

    added new routes to filter by collection and to check solr instalation status

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -51,8 +51,9 @@ Route::group(['prefix'=>'v1','as'=>'v1'], function(){
     });
 
     Route::group(['prefix' => 'catalogue'], function() {
-        Route::get('/{type}/index', [CatalogueController::class, 'index'])->name('catalogue.index');
-        Route::get('/{type}', [CatalogueController::class, 'get'])->name('catalogue.get');
+        Route::get('/checkSolr', [CatalogueController::class, 'checkSolr'])->name('catalogue.checkSolr');
+        Route::get('/{collection}/index', [CatalogueController::class, 'index'])->name('catalogue.index');
+        Route::get('/{collection}', [CatalogueController::class, 'get'])->name('catalogue.get');
         Route::delete('/clearAll', [CatalogueController::class, 'delete'])->name('catalogue.delete');
     });
 
