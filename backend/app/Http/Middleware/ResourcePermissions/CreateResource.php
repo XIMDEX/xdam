a744901ac38b72aeb582553dd4b2172bca78eb0e
Alvaro Rosado <alvaro.rosado93@gmail.com>
2021-03-03 15:48:06 +0100

    createResorce middleware provisionally skip permission checking

--- backend/app/Http/Middleware/ResourcePermissions/CreateResource.php
+++ backend/app/Http/Middleware/ResourcePermissions/CreateResource.php
@@ -23,6 +23,8 @@ class CreateResource
         $user = Auth::user();
 
         $workspace = Workspace::find($user->selected_workspace);
+        // Provisionally, the user can create any resource, within a collection
+        return $next($request);
 
         if($user->can(Abilities::CREATE_RESOURCE, $workspace) || $user->selected_workspace == null) {
             return $next($request);
