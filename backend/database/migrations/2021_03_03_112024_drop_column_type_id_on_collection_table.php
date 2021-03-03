6e872ada35972fb1f5b3297a6aa67b8b319e8e2c
Alvaro Rosado <alvaro.rosado93@gmail.com>
2021-03-03 15:46:40 +0100

    drop collection type table

---
+++ backend/database/migrations/2021_03_03_112024_drop_column_type_id_on_collection_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropColumnTypeIdOnCollectionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->dropColumn('type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
