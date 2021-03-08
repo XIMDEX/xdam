ac0efdf0d2dd1ce17ec10c640226d13494dcc357
Alvaro Rosado <alvaro.rosado93@gmail.com>
2021-03-08 15:30:10 +0100

    multimedia-mcgraw

--- backend/config/solarium.php
+++ backend/config/solarium.php
@@ -54,6 +54,18 @@ return [
             ],
             'resource' => 'BookSolrResource'
         ]
+        ,
+        'multimedia-mcgraw' => [
+            'endpoint' => [
+                'scheme' => 'http', # or https
+                'host' => env('SOLR_HOST', 'localhost'),
+                'port' => env('SOLR_PORT', '8983'),
+                'path' => env('SOLR_PATH', '/'),
+                'core' => 'multimedia-mcgraw',
+            ],
+            'resource' => 'MultimediaSolrResource'
+        ]
+
     ],
     'solr_validators_folder' => env('SOLR_VALIDATORS_FOLDER', ''),
     'solr_schemas_folder' => env('SOLR_SCHEMAS_FOLDER', ''),
