e452593b1c3f1f17d72c6aa35cacd7376052c618
Alvaro Rosado <alvaro.rosado93@gmail.com>
2021-03-11 13:28:51 +0100

    changed types for activities and assessments

--- backend/app/Http/Resources/Solr/ActivitySolrResource.php
+++ backend/app/Http/Resources/Solr/ActivitySolrResource.php
@@ -38,7 +38,7 @@ class ActivitySolrResource extends JsonResource
             'name' => $name ?? "",
             'data' => is_object($this->data) ? json_encode($this->data) : $this->data,
             'active' => $this->active,
-            'type' => ResourceType::fromValue($this->type)->key,
+            'type' => ResourceType::activity,
             'tags' => $this->tags()->pluck('name')->toArray() ?? [''],
             'categories' => $this->categories()->pluck('name')->toArray() ?? [''],
             'files' => $files,
--- backend/app/Http/Resources/Solr/AssessmentSolrResource.php
+++ backend/app/Http/Resources/Solr/AssessmentSolrResource.php
@@ -38,7 +38,7 @@ class AssessmentSolrResource extends JsonResource
             'name' => $name ?? "",
             'data' => is_object($this->data) ? json_encode($this->data) : $this->data,
             'active' => $this->active,
-            'type' => ResourceType::fromValue($this->type)->key,
+            'type' => ResourceType::assessment,
             'tags' => $this->tags()->pluck('name')->toArray() ?? [''],
             'categories' => $this->categories()->pluck('name')->toArray() ?? [''],
             'files' => $files,
