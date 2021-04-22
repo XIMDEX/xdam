fb1f75c20cf469e4b01bf02c4bc4aee32100a326
Cristian <schweizercristian@gmail.com>
2021-04-22 17:24:20 +0200

    updated script kakuma

--- backend/app/Console/Commands/Scripting/syncKakumaIds.php
+++ backend/app/Console/Commands/Scripting/syncKakumaIds.php
@@ -18,6 +18,8 @@ class syncKakumaIds extends Command
      */
     protected $signature = 'syncKakumaIds:start';
 
+    private $db_schema;
+
     /**
      * The console command description.
      *
@@ -45,24 +47,34 @@ class syncKakumaIds extends Command
      */
     public function handle(SolrService $solrService)
     {
+        $this->db_schema = env('DB_DATABASE');
 
         $resources = DamResource::where('type', 'course')->get();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
         $count = 0;
 
         foreach ($resources as $resource) {
             $current_resource_id = $resource->id;
 
-            $new_id = $this->update_dam_resource($resource);
+            $ur = $this->update_dam_resource($resource);
+            $new_id = $ur['new_id'];
+            $updated_resource = $ur['updated_resource'];
+
             $this->update_table('category_dam_resource', 'dam_resource_id', $current_resource_id, $new_id);
-            $this->update_table('dam_resource_uses', 'dam_resource_id', $current_resource_id, $new_id);
             $this->update_table('dam_resource_workspace', 'dam_resource_id', $current_resource_id, $new_id);
+            $this->update_table('dam_resource_uses', 'dam_resource_id', $current_resource_id, $new_id);
             $this->update_table('media', 'model_id', $current_resource_id, $new_id);
 
             //LAST STEP
-            $solrService->saveOrUpdateDocument($resource); //indexed
+
+            $solrService->saveOrUpdateDocument($updated_resource); //indexed
             $count++;
             $this->line("$count documents updated and indexed");
         }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
     }
 
@@ -73,20 +85,22 @@ class syncKakumaIds extends Command
         if (is_object($data)) {
             if (property_exists($data, 'description')) {
                 $resource->id = $data->id;
-                $resource->data = $data;
                 $resource->save(); //updated
             }
         }
 
-        return $data->id;
+        return ['new_id' => $data->id, 'updated_resource' => $resource];
     }
 
     public function update_table($table_name, $column_to_update, $current_resource_id, $new_id)
     {
-        $ress = DB::table($table_name)->where($column_to_update, $current_resource_id)->get();
-        foreach ($ress as $res) {
-            $res->dam_resource_id = $new_id;
-            $res->save();
-        }
+        echo $table_name . PHP_EOL;
+
+        $query = "UPDATE `".$this->db_schema."`.`".$table_name."` SET `".$column_to_update."` = '".$new_id."' WHERE (`".$column_to_update."` = '".$current_resource_id."');";
+
+        echo $query . PHP_EOL;
+
+        $exec = DB::statement($query);
+        echo $table_name . ' updated ' . PHP_EOL;
     }
 }
