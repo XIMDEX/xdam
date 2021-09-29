725cbc91b2d90fae42e54b65a88ca664edca2d17
Cristian <schweizercristian@gmail.com>
2021-09-29 18:13:20 +0200

    change in cost modifier command

--- backend/app/Console/Commands/Scripting/ParseCourseCostValue.php
+++ backend/app/Console/Commands/Scripting/ParseCourseCostValue.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands\Scripting;
 
 use App\Models\DamResource;
+use Exception;
 use Illuminate\Console\Command;
 
 class ParseCourseCostValue extends Command
@@ -18,28 +19,55 @@ class ParseCourseCostValue extends Command
 
     public function handle()
     {
-        $operator = $this->ask('operation (/ *)');
+        $operator = $this->ask('valid operators (+ - / * =)');
         $value = $this->ask('number');
 
         foreach (DamResource::where('type', 'course')->get() as $course) {
 
             $data = json_decode(json_encode($course->data), true);
 
-            if (isset($data['description']['cost'])) {
+            if (!isset($data['description']['cost'])) {
+                $cost_from_price = (isset($data['description']['price']) && $data['description']['price'] === '')
+                    ? 0
+                    : ($data['description']['price'] ?? 0);
 
-                $math = $operator === '/'
-                ? $data['description']['cost'] / $value
-                : $data['description']['cost'] * $value;
-
-                $data['description']['cost'] = (int)$math;
-                $course->data = $data;
-                $course->save();
-                $course->refresh();
-                echo "\n Success: $course->id cost is now " . $course->data->description->cost . PHP_EOL;
+                $data['description']['cost'] = $cost_from_price;
             }
 
+            $this->applyMath($course, $value, $operator, $data);
+
         }
-        echo "\n Finished: the rest of courses has not a cost property" . PHP_EOL;
+        echo "\n Finished" . PHP_EOL;
+    }
+
+    public function applyMath($course, $value, $operator, $data)
+    {
+        switch ($operator) {
+            case '=':
+                $math = $data['description']['cost'] = $value;
+                break;
+            case '/':
+                $math = $data['description']['cost'] / $value;
+                break;
+            case '*':
+                $math = $data['description']['cost'] * $value;
+                break;
+            case '-':
+                $math = $data['description']['cost'] - $value;
+                break;
+            case '+':
+                $math = $data['description']['cost'] + $value;
+                break;
+            default:
+                throw new Exception('Undefined operator');
+                break;
+        }
+
+        $data['description']['cost'] = (int)$math;
+        $course->data = $data;
+        $course->save();
+        $course->refresh();
+        echo "\n Success: $course->id cost is now " . $course->data->description->cost . PHP_EOL;
     }
 
 }
