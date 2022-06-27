51059d3794b65224b37234dcbe5f3bc32f548078
joan <jwcervera@hotmail.com>
2022-06-27 10:36:40 +0200

    Additional Batch Steps Enum added

---
+++ backend/app/Enums/AdditionalBatchSteps.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class AdditionalBatchSteps extends Enum
+{
+    const CONVERT_BOOKS_AFETER_UPDATE = "CONVERT_BOOKS_AFETER_UPDATE";
+}
