26b8dfb68db59ead9e5ef3643fa7bfbb4822ab43
Fran Enriquez <fran.enriquez.dev@gmail.com>
2022-08-23 19:36:40 +0200

    FIX: handle relative path on script delete and create cores solr

--- backend/solr_install_cores.sh
+++ backend/solr_install_cores.sh
@@ -10,15 +10,14 @@ delete_core() {
 create_core() {
 	# Gets the core and the user's argument
 	local core="$1"
-	local user="$2"
 
 	# Creates the Solr core
 	sudo su - solr -c "/opt/solr/bin/solr create -c $core -n data_driven_schema_configs"
 
 	# Stores the core's configuration
-	sudo cp /home/$user/htdocs/Projects/xdam/backend/storage/solr_core_conf/core_files/* /var/solr/data/$core
+	sudo cp ./storage/solr_core_conf/core_files/* /var/solr/data/$core
 	sudo chown -R solr:solr /var/solr/data/$core
-	sudo cp /home/$user/htdocs/Projects/xdam/backend/storage/solr_core_conf/core_conf_files/* /var/solr/data/$core/conf
+	sudo cp ./storage/solr_core_conf/core_conf_files/* /var/solr/data/$core/conf
 	sudo chown -R solr:solr /var/solr/data/$core/conf
 
 	# Installs the Solr core
@@ -26,16 +25,12 @@ create_core() {
 }
 
 
-# Asks for the username
-echo "Which is your current user?"
-read username
-
 # Sets the array with the cores to delete/create, and iterates through it
 cores=("activity" "assessment" "book" "course" "multimedia")
 for core in ${cores[@]}; do
 	# Deletes and creates the core
 	delete_core $core
-	create_core $core $username
+	create_core $core
 done
 
 # Reindexes the DB content
