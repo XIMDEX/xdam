9105b126b72cfc556998e3e7ba6e44e3075c5a1b
Lluís Masdeu <lluis.masdeu@ximdex.com>
2022-10-20 14:24:00 +0200

    [feature/OXE-1478] - Temporary update

--- backend/config/solarium.php
+++ backend/config/solarium.php
@@ -66,9 +66,19 @@ return [
             'resource' => 'BookSolrResource',
             'classHandler' => 'BookHandler',
             'accepts_types' => [ResourceType::book]
+        ],
+        'lom' => [
+            'endpoint' => [
+                'scheme' => 'http', # or https
+                'host' => env('SOLR_HOST', 'localhost'),
+                'port' => env('SOLR_PORT', '8983'),
+                'path' => env('SOLR_PATH', '/'),
+                'core' => 'lom',
+            ],
+            'resource' => 'LOMSolrResource',
+            'classHandler' => 'LOMHandler'
         ]
     ],
     'solr_validators_folder' => env('SOLR_VALIDATORS_FOLDER', ''),
     'solr_schemas_folder' => env('SOLR_SCHEMAS_FOLDER', ''),
-
 ];
