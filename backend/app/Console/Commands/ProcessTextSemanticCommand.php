778e5d8d04c42466b6378b49a3f6e2a06a2df80e
juandz13 <jdiaz@ximdex.com>
2023-08-11 18:01:55 +0200

    update

--- backend/app/Console/Commands/ProcessTextSemanticCommand.php
+++ backend/app/Console/Commands/ProcessTextSemanticCommand.php
@@ -5,8 +5,10 @@ namespace App\Console\Commands;
 use App\Services\ExternalApis\Xowl\XtagsCleaner;
 use App\Services\ExternalApis\XowlTextService;
 use Illuminate\Console\Command;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use stdClass;
 
 class ProcessTextSemanticCommand extends Command
 {
@@ -40,15 +42,26 @@ class ProcessTextSemanticCommand extends Command
      * @return int
      */
     public function handle()
-    {
-         $files = Storage::allFiles()[64];
-         $file = Storage::path($files);
+    {   
+         $object = new stdClass();
+         $object->uuid="";
+         $files = Storage::allFiles();
+         $finalFiles = [];
+        foreach ($files as $file) {
+           // var_dump($file);
+          if(isset(pathinfo($file)['extension']) && (pathinfo($file)['extension']==='txt' || pathinfo($file)['extension']==='pdf') ){
+            if(new UploadedFile($file,basename(dirname($file)))!==null)$finalFiles[] = new UploadedFile($file,basename(dirname($file)));
+          }
+        }
+        var_dump($finalFiles);
+       //  $file = Storage::path($files); 
        //  $file = Storage::get($files);
          //$passed = $this->get
          //$file22 = new \Symfony\Component\HttpFoundation\File\File($file);
-         $file = new \Symfony\Component\HttpFoundation\File\File($file);
-         var_dump($file->getRealPath());
-
+         //$file = new UploadedFile($file,basename(dirname($file)));
+         //$object->uuid=basename(dirname($file));
+         
+         //var_dump($this->getSemanticData($object,$file));
          echo("hello world\n");
         // var_dump($file);
         // $file = new \Symfony\Component\HttpFoundation\File\File($files);
@@ -56,9 +69,10 @@ class ProcessTextSemanticCommand extends Command
 
     }
 
-    private function GetSemanticData($description,$file){
+    private function getSemanticData($description,$file){
         $xowlText = new XowlTextService();
         $dataResult = $xowlText->getDataOwlFromFile($description,$file);
+        var_dump(  $dataResult );
         if($dataResult->status !=='FAIL'){
             $cleaner = new XtagsCleaner($dataResult->data->xtags,$dataResult->data->xtags_interlinked);
             return $cleaner->getProcessedXtags();
