9548916f68ae62315257bfeec8c4a6fb1a496cb6
juandz13 <jdiaz@ximdex.com>
2023-08-11 11:42:34 +0200

    command added

---
+++ backend/app/Console/Commands/ProcessTextSemanticCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\ExternalApis\Xowl\XtagsCleaner;
+use App\Services\ExternalApis\XowlTextService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+
+class ProcessTextSemanticCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'process:text';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+         $files = Storage::allFiles('public')[2];
+         echo("hello world\n");
+         var_dump($files);
+    }
+
+    private function GetSemanticData($description,$file){
+        $xowlText = new XowlTextService();
+        $dataResult = $xowlText->getDataOwlFromFile($description,$file);
+        if($dataResult->status !=='FAIL'){
+            $cleaner = new XtagsCleaner($dataResult->data->xtags,$dataResult->data->xtags_interlinked);
+            return $cleaner->getProcessedXtags();
+        }
+    }
+}
