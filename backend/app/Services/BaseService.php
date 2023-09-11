edd2c992a0d8f459219c1901803d459d3dc04ce2
juandz13 <jdiaz@ximdex.com>
2023-09-11 13:30:46 +0200

    tags and categories fix

--- backend/app/Services/BaseService.php
+++ backend/app/Services/BaseService.php
@@ -3,12 +3,27 @@
 declare(strict_types=1);
 
 namespace App\Services;
-
 class BaseService
 {
 
     public static function handleSchema($schema)
     {
+        foreach ($schema->properties->description->properties as $key => $property) {
+            if ($key === 'categories') {
+                $categories = CategoryService::where('type', 'course');
+                $schema->properties->description->properties->$key->options = $categories->all();
+                $schema->properties->description->properties->$key->subType = 'dropdown';
+            }
+            if ($key === 'semantic_tags') {
+                $schema->properties->description->properties->$key->subType = 'xtags';
+            }
+
+            if ($key === 'corporations') {
+                $corporations = CorporationService::getAll();
+                $schema->properties->description->properties->$key->options = $corporations;
+                $schema->properties->description->properties->$key->subType = 'dropdown';
+            }
+        }
         return $schema;
     }
 
