2ce0e81a90c0d39cccd8ce4c6e8958936b4b127a
FranEG80 <fran.enriquez.dev@gmail.com>
2024-04-11 12:15:25 +0200

    FEAT: add loading bar while load pdf on viewer pdf cdn resource

--- backend/public/viewerPDF/web/decode_ximdex_function.js
+++ backend/public/viewerPDF/web/decode_ximdex_function.js
@@ -1,10 +1,20 @@
 /**
  * Function obfuscated in final file of viewer.js
  */
+
 await (async function() {
-	const queryparams = new URLSearchParams(window.location.search);
+	let statusLoadBar = 0;
+	console.log('## LOADING XIMDEX PDF ##')
+	const incrementLoadbar = (loadBar, increment = 1) => {
+		if (statusLoadBar < 98) statusLoadBar += increment
+		if (statusLoadBar > 100) statusLoadBar = 100;
+		loadBar.style.setProperty("--progressBar-percent", statusLoadBar+"%");
+		console.log(statusLoadBar)
+	}
+	const loadBar = document.querySelector('#loadingBar')
+	// loadBar.classList.toggle('hidden');
+	incrementLoadbar(loadBar)
 	const hash = window.location.hash.substring(1);
-	console.log('## LOAD XIMDEX PDF ##')
 	const qp = new URLSearchParams(atob(hash))
 	if (qp.has('dx') && qp.get('dx') == 1) {
 		window.dx = true
@@ -13,8 +23,19 @@ await (async function() {
 
 	}
 	if (!window.ximdex) window.ximdex = {}
+	let loaded = false;
+	let time_interval = setInterval(() =>{
+		if (statusLoadBar < 95) {
+			incrementLoadbar(loadBar)
+		}
+	}, 500)
 	const i = await fetch(atob(hash))
 	const bl = await i.blob()
 	const url = URL.createObjectURL(bl)
-	window.ximdex.url = url}
-)();
+
+	clearInterval(time_interval)
+	incrementLoadbar(loadBar, 100)
+	loadBar.classList.toggle('hidden')
+	
+	window.ximdex.url = url
+})();
