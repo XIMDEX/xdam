779d9fbf1d0726481e4d1ba3fdf440f7c6f6bc67
juandz13 <jdiaz@ximdex.com>
2024-06-13 11:12:18 +0200

    manageorganization and workspace

--- backend/app/Http/Middleware/CanManageWorkspace.php
+++ backend/app/Http/Middleware/CanManageWorkspace.php
@@ -8,9 +8,15 @@ use App\Models\Workspace;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Lib\Xrole\Services\PermissionService;
 
 class CanManageWorkspace
 {
+
+    protected $permissionService;
+    public function __construct(PermissionService $permissionService){
+        $this->permissionService = $permissionService;
+    }
     /**
      * Handle an incoming request.
      *
@@ -22,7 +28,11 @@ class CanManageWorkspace
     {
         $wsp = Workspace::find($request->workspace_id);
         $user = Auth::user();
-
+        
+        if ($this->permissionService->isAdmin() || $this->permissionService->isSuperAdmin()) {
+            return $next($request);
+        }
+        return response()->json(['error_wsp' => 'Unauthorized.'], 401);
         $user_can_manage_the_organization_of_wsp = $user->canAny([Abilities::MANAGE_ORGANIZATION, Abilities::MANAGE_ORGANIZATION_WORKSPACES], $wsp->organization()->first());
         $user_can_manage_the_workspace = $user->canAny([Abilities::MANAGE_WORKSPACE, Abilities::UPDATE_WORKSPACE], $wsp);
 
