795b1045ebe9d36993b79321caec355280dac294
juandz13 <jdiaz@ximdex.com>
2024-06-25 12:28:28 +0200

    xdir controller

--- backend/app/Http/Controllers/XdirController.php
+++ backend/app/Http/Controllers/XdirController.php
@@ -2,12 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class XdirController extends Controller
 {
-    
-    public function action(Request $request){
-        //do something 
+    private $actions;
+
+    public function __construct()
+    {
+        $this->actions = [
+            'createUser' => 'createUser'
+        ];
+    }
+
+    public function action(Request $request)
+    {
+        $actionName = $request->action;
+        $data = $request->data;
+
+        if (method_exists($this, $actionName)) {
+            if (is_string($actionName) && method_exists($this, $actionName)) {
+                return $this->{$actionName}($data);
+            } else {
+                throw new Exception("Method name must be a string and a valid callable method.");
+            }
+        } else {
+            throw new Exception("Action {$actionName} not found.");
+        }
+    }
+
+    private function createUser($data)
+    {
+        try {
+            $user = User::where('email', $data['user']['email'])->first();
+            if ($user) {
+                return response()->json(['success' => true], Response::HTTP_OK);
+            }
+            $data['user']['password'] = Hash::make("pb0HuH9Ep+ThjvC;hz:T£3`V'A");
+            
+            $user = User::create($data['user']);
+            $user->save();
+            return response()->json(['success' => true], Response::HTTP_OK);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage(), 'success' => false], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private function checkIfUserExists($data)
+    {
+        $data = json_decode($data);
+        $user = User::FindOrFail($data->id);
+
+        if ($user) {
+            return response()->json(['exists' => true], Response::HTTP_OK);
+        }
+
+        return response()->json(['exists' => false], Response::HTTP_OK);
     }
 }
