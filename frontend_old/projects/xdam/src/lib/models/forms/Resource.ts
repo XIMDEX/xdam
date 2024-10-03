e54bc8493747cc8b91b33573367ec0cab4e2c624
Juan Prieto <juanpri@golem.giroscopo.com>
2024-10-03 10:59:33 +0200

    LICENSE and Guide updating

--- frontend_old/projects/xdam/src/lib/models/forms/Resource.ts
+++
@@ -1,7 +0,0 @@
-export class Resource {
-    public id: string;
-    public name: string;
-    public category: string;
-    public description: string;
-    public preview: string;
-}
--- frontend_old/projects/xdam/src/models/src/public_api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of xdam
- */
-
-export * from './lib';
