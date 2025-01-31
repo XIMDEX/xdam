b5e9638c56381a24953c60dcb6dcd21b15d7b40d
juandz13 <jdiaz@ximdex.com>
2025-01-31 11:54:26 +0100

    error hanlde added

--- backend/app/Http/Controllers/ResourceAmazonController.php
+++ backend/app/Http/Controllers/ResourceAmazonController.php
@@ -41,23 +41,27 @@ class ResourceAmazonController extends Controller
      */
     public function save(Request $request, $cdnCode)
     {
-        if (($cdn = $this->cdnService->getCDNInfo($cdnCode)) === null) return response(['error' => 'The CDN doesn\'t exist.']);
-        if ($cdn->isCollectionAccessible($request->collection_id) === false) return response(['error' => 'The collection isn\'t accessible for this CDN.']);
-        
-        $metadataString = ($request->metadata);
+        try {
+            if (($cdn = $this->cdnService->getCDNInfo($cdnCode)) === null) throw new \Exception('The CDN doesn\'t exist.');
+            if ($cdn->isCollectionAccessible($request->collection_id) === false) throw new \Exception('The collection isn\'t accessible for this CDN.');
+            
+            $metadataString = ($request->metadata);
 
-        $tmpFilePath = tempnam(sys_get_temp_dir(), 'metadata_') . '.txt';
+            $tmpFilePath = tempnam(sys_get_temp_dir(), 'metadata_') . '.txt';
 
-        file_put_contents($tmpFilePath, $metadataString);
-        $remoteFile = $this->getAmazonResourceService->getResourceByCurl($request->urlFile);
-        $files['File'] = $remoteFile;
-        $type = Collection::find($request->collection_id)->accept;
-        $resource = ($this->saveAmazonResourceService->save($request->urlFile, $request->nameFile, $request->metadata, $request->collection_id,$type, $request->workspace_id,  $files));
-        $resource->addMedia($tmpFilePath)->toMediaCollection('File');
+            file_put_contents($tmpFilePath, $metadataString);
+            $remoteFile = $this->getAmazonResourceService->getResourceByCurl($request->urlFile);
+            $files['File'] = $remoteFile;
+            $type = Collection::find($request->collection_id)->accept;
+            $resource = ($this->saveAmazonResourceService->save($request->urlFile, $request->nameFile, $request->metadata, $request->collection_id,$type, $request->workspace_id,  $files));
+            $resource->addMedia($tmpFilePath)->toMediaCollection('File');
 
-        $url =  $resource->id;
-        return response(['resource_id' => $url])
-            ->setStatusCode(Response::HTTP_OK);
+            $url =  $resource->id;
+            return response(['resource_id' => $url])
+                ->setStatusCode(Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response(['error' => 'error saving resource'], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -69,9 +73,13 @@ class ResourceAmazonController extends Controller
      */
     public function getUrls($cdnCode, $damResource)
     {
-        $resource = $this->getCDNResourceService->getResourceUrls($cdnCode, $damResource);
-        return  response($resource)
-            ->setStatusCode(Response::HTTP_OK);
+        try {
+            $resource = $this->getCDNResourceService->getResourceUrls($cdnCode, $damResource);
+            return  response($resource)
+                ->setStatusCode(Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -83,9 +91,13 @@ class ResourceAmazonController extends Controller
      */
     public function getResource($cdnCode, $damResource)
     {
-        $resource = $this->getCDNResourceService->getResourceInfo($cdnCode, $damResource);
-        return  response($resource)
-            ->setStatusCode(Response::HTTP_OK);
+        try {
+            $resource = $this->getCDNResourceService->getResourceInfo($cdnCode, $damResource);
+            return  response($resource)
+                ->setStatusCode(Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response(['error' => 'error getting resource'], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -95,7 +107,11 @@ class ResourceAmazonController extends Controller
      */
     public function getWorkspaces()
     {
-        return Workspace::all();
+        try {
+            return Workspace::all();
+        } catch (\Exception $e) {
+            return response(['error' => 'error getting workspaces'], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -107,9 +123,13 @@ class ResourceAmazonController extends Controller
      */
     public function assignWorkspace($isbn, $workspaceId)
     {
-        $resources = $this->categoryService->getResources(Category::where('name', $isbn)->first(), true);
-        $this->assignWorkspaceService->assignWorkspace($workspaceId, $resources);
-        return response(['message' => 'Resources assigned to workspace ' . $workspaceId . ' successfully'], Response::HTTP_OK);
+        try {
+            $resources = $this->categoryService->getResources(Category::where('name', $isbn)->first(), true);
+            $this->assignWorkspaceService->assignWorkspace($workspaceId, $resources);
+            return response(['message' => 'Resources assigned to workspace ' . $workspaceId . ' successfully'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response(['error' => 'error assigning resources'], Response::HTTP_BAD_REQUEST);
+        }
     }
 
     /**
@@ -121,8 +141,13 @@ class ResourceAmazonController extends Controller
      */
     public function unassignWorkspace($isbn, $workspaceId)
     {
-        $resources = $this->categoryService->getResources(Category::where('name', $isbn)->first(), true);
-        $this->assignWorkspaceService->unassignWorkspace($workspaceId,$resources );
-        return response(['message' => "Resources with ISBN: $isbn deassigned from workspace $workspaceId successfully"], Response::HTTP_OK);
+        try {
+            $resources = $this->categoryService->getResources(Category::where('name', $isbn)->first(), true);
+            $this->assignWorkspaceService->unassignWorkspace($workspaceId,$resources );
+            return response(['message' => "Resources with ISBN: $isbn deassigned from workspace $workspaceId successfully"], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return response(['error' => 'error deassigning resources'], Response::HTTP_BAD_REQUEST);
+        }
     }
 }
+
