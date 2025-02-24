50ab0ce7f6750bb835aba206135c02b420ac315e
juandz13 <jdiaz@ximdex.com>
2025-02-24 19:17:11 +0100

    s3 first

--- backend/app/Services/Amazon/GetAmazonResourceService.php
+++ backend/app/Services/Amazon/GetAmazonResourceService.php
@@ -2,36 +2,57 @@
 
 namespace App\Services\Amazon;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Contracts\Filesystem\Filesystem;
 
 
 class GetAmazonResourceService
 {
-    public static function getResourceByCurl(string $url)
+    private Filesystem $disk;
+
+    public function __construct()
     {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-        $resourceContent = curl_exec($ch);
-        if ($resourceContent === false) {
-            curl_close($ch);
-            throw new \Exception('Failed to retrieve resource from URL: ' . $url);
+        $this->disk = Storage::disk('s3');
+    }
+
+    public function getResource(string $s3Key): UploadedFile
+    {
+        $this->validateFileExists($s3Key);
+        
+       // $contentType = $this->disk->mimeType($s3Key);
+        $contentLength = $this->disk->size($s3Key);
+        $resourceContent = $this->disk->get($s3Key);
+
+        $tempFilePath = $this->createTempFile($resourceContent);
+
+        return $this->createUploadedFile($tempFilePath, $s3Key, 'image/jpeg', $contentLength);
+    }
+
+    private function validateFileExists(string $s3Key): void
+    {
+        if (!$this->disk->exists($s3Key)) {
+            throw new FileNotFoundException("File not found in S3: {$s3Key}");
         }
-        $info = curl_getinfo($ch);
-        curl_close($ch);
+    }
 
-        if ($info['http_code'] != 200) {
-            throw new \Exception('Resource not found or access denied for URL: ' . $url);
-        }
+    private function createTempFile(string $content): string
+    {
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'S3File');
+        file_put_contents($tempFilePath, $content);
+        return $tempFilePath;
+    }
 
-        $tempFilePath = tempnam(sys_get_temp_dir(), 'remoteFile');
-        file_put_contents($tempFilePath, $resourceContent);
-
-        return new \Illuminate\Http\UploadedFile(
-            $tempFilePath,
-            basename(parse_url($url, PHP_URL_PATH)),
-            $info['content_type'],
-            $info['size_download'],
+    private function createUploadedFile(string $path, string $originalName, string $mimeType, int $size): UploadedFile
+    {
+        return new UploadedFile(
+            $path,
+            basename($originalName),
+            $mimeType,
+            $size,
             UPLOAD_ERR_OK,
+            true
         );
     }
 }
